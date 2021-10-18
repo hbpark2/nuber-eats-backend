50787789dfbbc38bff9957f025712c87e179abf7
hbpark2 <64389093+hbpark2@users.noreply.github.com>
2021-10-18 14:49:46 +0900

    dto

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,16 @@
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurant.entity';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+
+@Resolver((of) => Restaurant)
+export class RestaurantResolver {
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
+    console.log(createRestaurantDto);
+    return true;
+  }
+}
